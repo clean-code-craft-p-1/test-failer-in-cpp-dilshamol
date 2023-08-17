3eabaf1dc895919c66071fe66eb46bb827d89cb1
dilshamol <140040094+dilshamol@users.noreply.github.com>
2023-08-17 09:35:08 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,10 +3,10 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if (cms < 38) {
+    if (cms <= 38) {
         sizeName = 'S';
     }
-    else if (cms > 38 && cms < 42) {
+    else if (cms > 38 && cms <= 42) {
         sizeName = 'M';
     }
     else if (cms > 42) {
